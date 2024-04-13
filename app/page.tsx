a41b6dd80729dbf191caf39baa6a02e1a3627af2
Tanuj <tanujnayal2909@gmail.com>
2024-04-13 09:19:01 +0530

    added frontend and deploy script

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,71 @@
 "use client";
 
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 import { useEffect, useState } from "react";
 
 export default function Home() {
 
   const [input, setInput] = useState('');
-  const [results, setResults] = useState<{
+  const [searchResults, setSearchResults] = useState<{
     results: string[];
     duration: number
   }>();
 
   useEffect(() => {
     const fetchData = async () => {
-      if(!input) return setResults(undefined);
+      if(!input) return setSearchResults(undefined);
 
-      const res = await fetch(`/api/search?q=${input}`);
+      const res = await fetch(`https://search-api.tanujnayal2909.workers.dev/api/search?q=${input}`);
+      const data = (await res.json()) as {
+        results: string[]; 
+        duration:number;
+      }
+      setSearchResults(data);
     }
     fetchData()
   }, [input])
 
   return (
-    <div>
-      <input 
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        type="text"
-        className="text-zinc-500"
-      />
-    </div>
+    <main className="h-screen w-screen grainy">
+      <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
+        <h1 className="text-5xl tracking-tight font-bold">Search</h1>
+        <p className="text-zinc-600 text-lg max-w-prose text-center">Made using Hono, Next and Redis</p>
+        <div className="max-w-md w-full">
+          <Command>
+            <CommandInput 
+              value={input}
+              onValueChange={setInput}
+              placeholder='Search countries...'
+              className="placeholder:text-zinc-500"
+            />
+            <CommandList>
+              {searchResults?.results.length === 0 ? (
+                <CommandEmpty>No results found.</CommandEmpty>
+              ) : null}
+              {searchResults?.results ? (
+                <CommandGroup heading="results">
+                  {searchResults?.results.map((result) => (
+                    <CommandItem 
+                      key={result} 
+                      value={result}
+                      onSelect={setInput}
+                    >
+                      {result}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              ) : null}
+              {searchResults?.results ? (
+                <>
+                  <div className="h-px w-full bg-zinc-200"/>
+                  <p className="p-2 text-xs text-zinc-500">Found {searchResults.results.length} results in {searchResults.duration.toFixed(0)}ms</p>
+                </>
+              ) : null}
+            </CommandList>
+          </Command>
+        </div>
+      </div>
+    </main>
   );
     
 }
